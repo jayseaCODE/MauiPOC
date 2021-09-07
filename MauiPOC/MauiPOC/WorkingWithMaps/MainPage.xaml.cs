2b5ccc4a792b910f96c596a5dab825b9c89954b2
Joshua Chen <joshua.chen@valocityglobal.com>
2021-09-07 19:57:03 +1200

    more edits

--- MauiPOC/MauiPOC/WorkingWithMaps/MainPage.xaml.cs
+++ MauiPOC/MauiPOC/WorkingWithMaps/MainPage.xaml.cs
@@ -4,11 +4,11 @@ using Microsoft.Maui; using Microsoft.Maui.Controls;
 
 namespace MauiPOC.View
 {
-    public partial class MainPage : ContentPage
+    public partial class WorkingWithMapsPage : ContentPage
     {
         public ICommand NavigateCommand { get; set; }
 
-        public MainPage()
+        public WorkingWithMapsPage()
         {
             InitializeComponent();
 
